b86683229521aa3b8e4227b2c56deb8c2fbe5fbe
MariaP78 <maria_minodora07@yahoo.com>
2021-07-15 18:45:10 +0300

    Adunare Junit Maria

---
+++ src/main/java/Adunare.java
@@ -0,0 +1,5 @@
+public class Adunare {
+    public double suma(double a, double b){
+        return a+b;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Adunare add = new Adunare();
+
+        System.out.println(add.suma(2,3));
+    }
+}
